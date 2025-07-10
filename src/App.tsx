2e70ce227a47fbc5e4de585f184eacabfb6bb7ac
hakawa2 <hakawa.alves@gmail.com>
2025-07-10 20:30:49 -0300

    feat(heading): recive props in heading component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Heading } from './components/Heading/Heading';
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>Olá oswaldo</Heading>
+
+      <Heading>Olá mundo 2</Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
         voluptatem veniam, quia ex laborum itaque ab rerum consequatur vitae
