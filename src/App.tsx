e2e13b2a87b7334fe20ae223cfbd373863492253
hakawa2 <hakawa.alves@gmail.com>
2025-07-11 19:36:21 -0300

    wip(container): create a container component without styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,3 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading/Heading';
-
 export function App() {
-  return (
-    <>
-      <Heading>
-        Olá oswaldo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-        voluptatem veniam, quia ex laborum itaque ab rerum consequatur vitae
-        corporis quos, nulla a neque fugit? At consequatur sit quia et!
-      </p>
-    </>
-  );
+  return <></>;
 }
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,5 @@
+import styles from './Container.module.css';
+
+export function Container() {
+  return <section className={styles.container}></section>;
+}
